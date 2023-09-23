1ad0ef926bd93242853a4e155dba6c9de31c647e
Imuhammadtaha <145789772+Imuhammadtaha@users.noreply.github.com>
2023-09-23 13:57:38 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/Ecommerece', {
+    const conn = await mongoose.connect('mongodb://localhost:27017/Ecommerece', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
