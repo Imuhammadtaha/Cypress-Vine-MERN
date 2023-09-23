ea92205efe08ea89b22e9f7e78c8766146a14bba
Imuhammadtaha <145789772+Imuhammadtaha@users.noreply.github.com>
2023-09-23 14:33:14 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect('mongodb://0.0.0.0:27017/Ecommerece', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log(`Hurray Connected to MongoDB Database ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error in MongoDB: ${error}`);
-  }
-};
-
+const connectDB = async ()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology: true
+        });
+        console.log(`Hurray Connected to Mongodb Database ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error in Mongo Db ${error}`);
+        
+    }
+}
 export default connectDB;
